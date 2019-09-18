f9c8e5bf14d84c652bb46e16bc6777fd4ab0d6f0
Jessica D'Cunha <35900289+jrdcunha@users.noreply.github.com>
2019-09-17 20:53:20 -0600

    add button starts new activity

--- app/src/main/java/com/example/jdcunha_ridebook/RideListActivity.java
+++ app/src/main/java/com/example/jdcunha_ridebook/RideListActivity.java
@@ -2,9 +2,11 @@ package com.example.jdcunha_ridebook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -50,5 +52,17 @@ public class RideListActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                Intent i = new Intent(this, ViewEditRideActivity.class);
+                this.startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
